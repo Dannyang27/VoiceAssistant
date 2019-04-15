596f9b367d662362124848d3df050c9551f85af2
Le Danny <danny27995@gmail.com>
2019-04-15 18:38:58 +0200

    bit of refactoring

--- app/src/main/java/com/example/voiceassistant/Util/VoiceController.kt
+++ app/src/main/java/com/example/voiceassistant/Util/VoiceController.kt
@@ -16,40 +16,9 @@ object VoiceController{
     const val CITY_WEATHER = "TELL WE THE WEATHER IN"
     val RAINING = listOf("SHOULD I GET AN UMBRELLA", "IS IT RAINING")
 
-    val cityHashmap = mapOf(
-        Pair("Barcelona", 6356055),
-        Pair("Londres", 6058560),
-        Pair("Denia", 2518878),
-        Pair("Valencia", 2509954),
-        Pair("Madrid", 6359304),
-        Pair("Amsterdam", 2759794),
-        Pair("Tokio", 1850147),
-        Pair("Paris", 6455259),
-        Pair("Berlin", 2950158)
-    )
-
-    fun getIdByCity(words: List<String>) : Int{
-    Log.d(RetrofitClient.TAG, words.toString())
-        for (word in words){
-            for(key in cityHashmap.keys){
-                if(key == word){
-                    return cityHashmap[key]!!
-                }
-            }
-        }
-
-        return -1
-    }
-
     fun processVoiceInput(voiceInput: String): String{
-        // Create string and add to VoiceAssistaceFragment.Adapter
-//        VoiceAssistanceFragment.messages.add(Message(VoiceAssistanceFragment.messages.size, Sender.BOT, "Sure!"))
-//        VoiceAssistanceFragment.viewAdapter.notifyDataSetChanged()
 
         var response = ""
-        Log.d(RetrofitClient.TAG, voiceInput.toUpperCase())
-        Log.d(RetrofitClient.TAG, CITY_WEATHER)
-
 
         if( voiceInput.toUpperCase() in CITY_WEATHER){
             Log.d(RetrofitClient.TAG, "SPLIT:" +  voiceInput.split(" ").toString())
@@ -85,8 +54,8 @@ object VoiceController{
             else -> response = "Sorry, I did not get it"
 
         }
-        VoiceAssistanceFragment.addMessage(Message(VoiceAssistanceFragment.messages.size, Sender.BOT, response))
 
+        VoiceAssistanceFragment.addMessage(Message(VoiceAssistanceFragment.messages.size, Sender.BOT, response))
         return response
     }
 }
