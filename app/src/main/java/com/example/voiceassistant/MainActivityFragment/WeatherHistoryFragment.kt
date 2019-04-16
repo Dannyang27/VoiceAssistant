96c8e097d9dd6509639b6f79f54e6111c83d9d5a
Le Danny <danny27995@gmail.com>
2019-04-16 19:52:58 +0200

    Historial fragment initialized

--- app/src/main/java/com/example/voiceassistant/MainActivityFragment/WeatherHistoryFragment.kt
+++ app/src/main/java/com/example/voiceassistant/MainActivityFragment/WeatherHistoryFragment.kt
@@ -2,12 +2,19 @@ package com.example.voiceassistant.MainActivityFragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.voiceassistant.Adapter.HistorialAdapter
 import com.example.voiceassistant.R
 
 class WeatherHistoryFragment : Fragment(){
+
+    private lateinit var historialRV: RecyclerView
+    private lateinit var viewManager : RecyclerView.LayoutManager
+    private lateinit var viewAdapter : RecyclerView.Adapter<*>
 
     companion object {
         fun newInstance(): WeatherHistoryFragment = WeatherHistoryFragment()
@@ -15,6 +22,16 @@ class WeatherHistoryFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.history_fragment, container, false)
+
+        viewManager = LinearLayoutManager(activity)
+        viewAdapter = HistorialAdapter(mutableListOf())
+
+        historialRV = view.findViewById<RecyclerView>(R.id.weather_history_rv).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
         return view
     }
 }
