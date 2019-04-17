c20880941a6aea0a623c1f337eed965712aa41a6
Le Danny <danny27995@gmail.com>
2019-04-17 16:41:50 +0200

    Calendarview get selected day

--- app/src/main/java/com/example/voiceassistant/MainActivityFragment/CalendarFragment.kt
+++ app/src/main/java/com/example/voiceassistant/MainActivityFragment/CalendarFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
 import com.example.voiceassistant.R
 
 class CalendarFragment : Fragment(){
@@ -15,6 +17,15 @@ class CalendarFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.calendar_fragment , container, false)
+
+        val calendar = view.findViewById<CalendarView>(R.id.calendar)
+
+        calendar.setOnDateChangeListener{view, year, month, dayOfMonth ->
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+
+        }
+
         return view
     }
 }
