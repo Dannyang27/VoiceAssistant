20511c3ab45d0b2d85a95cceabc0fdbc12ea2aed
Le Danny <danny27995@gmail.com>
2019-05-16 14:28:30 +0200

    Toolbar added to PreferenceSettings

--- app/src/main/java/com/example/voiceassistant/SettingsActivity.kt
+++ app/src/main/java/com/example/voiceassistant/SettingsActivity.kt
@@ -3,6 +3,8 @@ package com.example.voiceassistant
 import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceManager
+import android.support.v7.widget.Toolbar
+import android.util.TypedValue
 import android.widget.Toast
 import com.example.voiceassistant.Database.WeatherRepository
 import com.example.voiceassistant.MainActivityFragment.WeatherHistoryFragment
@@ -10,6 +12,11 @@ import com.example.voiceassistant.MainActivityFragment.WeatherHistoryFragment
 class SettingsActivity: AppCompatPreferenceActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        layoutInflater.inflate(R.layout.toolbar, findViewById(android.R.id.content))
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        fixPadding()
+        setSupportActionBar(toolbar)
         setupActionBar()
 
         PreferenceManager.setDefaultValues(this, R.xml.pref_general, false)
@@ -26,5 +33,21 @@ class SettingsActivity: AppCompatPreferenceActivity(){
     }
     private fun setupActionBar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
+
+
+    private fun fixPadding(){
+        val horizontalMargin =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2f, resources.displayMetrics).toInt()
+        val verticalMargin =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2f, resources.displayMetrics).toInt()
+        val topMargin = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            (resources.getDimension(R.dimen.preference_padding).toInt() + 30).toFloat(),
+            resources.displayMetrics
+        ).toInt()
+        listView.setPadding(horizontalMargin, topMargin, horizontalMargin, verticalMargin)
+    }
+
 }
